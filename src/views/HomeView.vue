e55a5badbb96358ed8c283c4b87669aac6273242
giuli9r <giuli.rossetti9@gmail.com>
2024-09-17 21:12:12 -0300

    add created hook

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,34 +2,61 @@
 import { useLoginStore } from '@/stores/loginStore.js';
 
 export default {
+  data() {
+    return {
+      isLogged: true, // inicialmente true
+      loginStore: useLoginStore() // Referencia unica
+    };
+  },
+  created() {
+    const loginStore = useLoginStore();
+    // Verificar el estado del login al montar el componente
+    loginStore.checkLocalStorage();
+    // Sincronizar la variable isLogged con el estado del loginStore
+    this.isLogged = loginStore.isLogged;
+  },
   methods: {
     handleLogout() {
-      const loginStore = useLoginStore();
-      loginStore.logout();
+      this.loginStore.logout();
+      this.$router.push('/login')
     }
-  }
-};
+  },
 
-// idea - buscar si el usuario ya se logueo, al momento de montar el componente
-// methods: {
-//     checkLocalStorageSessionState() {
-//       return localStorage.getItem('loginStore.isLogged')
-//     }
-//   },
-//   mounted() {
-//     // methods can be called in lifecycle hooks, or other methods!
-//     this.checkLocalStorageSessionState()
-//   }
+  // idea - buscar si el usuario ya se logueo, al momento de montar el componente
+  // methods: {
+  //     checkLocalStorageSessionState() {
+  //       return localStorage.getItem('loginStore.isLogged')
+  //     }
+  //   },
+  //   mounted() {
+  //     // methods can be called in lifecycle hooks, or other methods!
+  //     this.checkLocalStorageSessionState()
+  //   }
+
+  watch: {
+      'loginStore.isLogged'(newValue) {
+        this.isLogged = newValue;
+      }
+    }
+};
 
 </script>
 
 <template>
-    <h3> Msg: {{ msg }}</h3>
-    <h2>
+    <!-- <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+    </nav>
+    <router-view></router-view> -->
+    
+    <div>
+      <h3> Msg: {{ msg }}</h3>
+      <h2>
         Hi! Welcome to HomeView,
-    </h2>
-    <p> file inside template defined in /views/HomeView </p>
-    <button @click="handleLogout">Cerrar sesión</button>
+      </h2>
+      <button @click="handleLogout">Cerrar sesión</button>
+    </div>
+
 </template>
 
 <style scoped>
