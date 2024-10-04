58b80947699a1290ae0e8123b93b098b2526070e
giuli9r <giuli.rossetti9@gmail.com>
2024-10-03 21:33:09 -0300

    new store access. unique instance in data()

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,66 +1,50 @@
 <script>
-import { useLoginStore } from '@/stores/loginStore'
-import {  mapStores, mapState, mapActions  } from 'pinia'
+import { useLoginStore } from '@/stores/loginStore.js'
+import LogoutComponent from '@/components/LogoutComponent.vue'
 
 export default {
   data() {
     return {
-      username: ''
-    };
+      store: useLoginStore() // unica instancia de  id+Store.js en el componente
+    }
   },
-  components: {
-  },
+  components: { LogoutComponent },
   computed: {
-    // each store will be accessible as its id + 'Store' (loginStore)
-    ...mapState(useLoginStore, ['username', 'password', 'isLoggedIn' ]),
-    ...mapStores(useLoginStore),    // loginStore.isLoggedIn, loginStore.showUserName, loginStore.username
+    username() {
+      return this.store.username // forma para recuperar state del Store.js
+    },
+    isLoggedIn() {
+      return this.store.isLoggedIn
+    }
   },
   methods: {
-    ...mapActions(useLoginStore, [ 'logout',
-                                  'checkLocalStorage',
-      ]),
-    showStoreHome(){
-      console.log("Show Login Store from Home btn");
-      console.log(this.loginStore.isLoggedIn);
-      console.log(this.loginStore.username);
-      console.log(this.loginStore.password);
-    },
-    showFromPinia(){
-      this.loginStore.showStore();
-    },
+    increment() {
+      this.store.increment() // forma para recuperar actions del Store.js
+    }
   },
   mounted() {
     console.log(`The HomeView  mounted.`)
-    this.username = localStorage.getItem('username')?? 'ASD';
-    // this.loginStore.loggedIn()
-  },
-
-};
-
+  }
+}
 </script>
 
 <template>
   <div>
-      <div id="main-content">
-        <div class="greetings">
-          <!-- <h1 class="green">You are logged in as {{ loginStore.username }}</h1> -->
-          <h1 class="green">You are logged in as {{ this.username }}</h1>
-          <h3>
-            You can 
-            <span class="a2">buy</span> and
-            <span class="a2">sale</span> with ease, but at your own risk.
-          </h3>
-          <button @click="showStoreHome"> Click to console store From HOME</button>
-          <button @click="showFromPinia"> Click to console store From PINIA</button>
+    <div id="main-content">
+      <div class="greetings">
+        <!-- <h1 class="green">You are logged in as {{ loginStore.username }}</h1> -->
+        <h1 class="green">You are logged in as {{ this.username }}</h1>
+        <h3>
+          You can
+          <span class="a2">buy</span> and <span class="a2">sale</span> with ease, but at your own
+          risk.
+        </h3>
+        <div>
+          <logout-component />
+        </div>
       </div>
-      <!-- <LoginView v-if="!this.loginStore.isLoggedIn"/>
-      <TransactionsView  v-if="this.loginStore.isLoggedIn" />
-      <StatisticsView  v-if="this.loginStore.isLoggedIn" />
-      <HelpView  v-if="this.loginStore.isLoggedIn"/>  -->
     </div>
-      <!-- <router-view /> -->
   </div>
-   
 </template>
 
 <style scoped>
@@ -88,13 +72,4 @@ h3 {
 .greetings h3 {
   text-align: center;
 }
-
-/* @media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-} */
-
 </style>
-
