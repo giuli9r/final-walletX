9cd800e5f616eaa147d4039fee4b1ef61811934e
giuli9r <giuli.rossetti9@gmail.com>
2024-10-08 22:27:04 -0300

    funcionalities added: updateWalletState, addTransaction logic, persist key

--- src/stores/transactionStore.js
+++ src/stores/transactionStore.js
@@ -13,45 +13,66 @@ export const useTransactionStore = defineStore('transaction', {
   
   state: () => ({
     transactions: [],
-    wallet: { },
+    wallet: {
+      btc: null,
+      eth: null,
+      dai: null,
+      sol: null,
+      usdt: null,
+      fiatAmount: 0
+     },
 
   }),
 
   getters: {
-    getWallet(state) {
-      return state.wallet
+    getTransactions(state) {
+      return state.transactions
     }
   },
   
   actions: {
 
-    updateWalletState ( CRYPTO_CODE, CRYPTO_AMOUNT, ACTION ) {
-      switch (ACTION) {
-        case 'purchase':
-          apiClient.get();
-          break;
-      
-        case 'sell':
-        
-        break;
-        default:
-          break;
-      }
+    updateWalletState ( transactionObj ) {
+      let action = transactionObj.action
+      let crypto_code = transactionObj.crypto_code  // siempre minuscula
+      let crypto_amount = (action === 'purchase') ? transactionObj.crypto_amount : -transactionObj.crypto_amount;
+      let fiatAmount = (action === 'purchase') ?  transactionObj.money : -transactionObj.money;
+
+      // Inicializamos btc/eth/sol... si es null
+      this.wallet[crypto_code] = this.wallet[crypto_code] || {};
+      this.wallet[crypto_code]['amount'] += crypto_amount;
+      this.wallet.fiatAmount += fiatAmount;
+  
       return
     },
 
-    addTransaction(transactionObj) {
+    async addTransaction(transactionObj) {
       try {
-        this.transactions.push(transactionObj)
-        /**
-           CALL TO API WITH AXIOS.POST
-          CRYPTO_CODE, CRYPTO_AMOUNT, FIAT_AMOUNT ,ACTION
+        console.log('TransactionStore Data:', transactionObj);
+        /** console.log
+          user_id: username,  action: action,  crypto_code: this.selectedCrypto,
+          crypto_amount: this.cryptoAmount, money: this.fiatAmount,  datetime: new Date(),
         */
-        console.log("transaction added")
+        console.log("Checking availability....")
+        this.checkingAvailability(transactionObj);
+        console.log('POST REQUEST IN PROGRESS....'); // add realoading
+        const response = await apiClient.post('', transactionObj);
+        console.log('POST RESPONSE READY');
+
+        // add transaction to local storage and update wallet values
+        this.transactions.push(transactionObj)
+        this.updateWalletState(transactionObj);
+        console.log("transaction added, wallet state and transaction-key storage updated ")
+        console.log('Response Data:', response.data);
+        return response.data;
+
       } catch (error) {
+        console.error('Error when adding new transaction:', error);
+        if (error.response) {
+          console.log('Server Response:', error.response.data);
+        }
         console.log(error)
       }
-
     },
 
     deleteTransaction(transactionID){
@@ -80,7 +101,19 @@ export const useTransactionStore = defineStore('transaction', {
       } catch (error) {
         console.log(error)
       }
+    },
+
+    checkingAvailability( transactionObj) {
+      console.log(transactionObj)
+      console.log("You can continue.")
+      return
     }
 
   },
+
+  persist: {
+    key: 'transaction-key',
+    storage: localStorage,
+  },
+
 })
