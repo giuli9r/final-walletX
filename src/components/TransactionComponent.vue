3e0dd7ee20bc8d238f5a48c2c488d2cfa2e27911
giuli9r <giuli.rossetti9@gmail.com>
2024-10-18 22:38:56 -0300

    fix typo

--- src/components/TransactionComponent.vue
+++ src/components/TransactionComponent.vue
@@ -25,7 +25,7 @@ export default {
       cryptos: [
         { name: 'Bitcoin', code: 'BTC'}, 
         { name: 'Ethereum', code: 'ETH'}, 
-        { name: 'DAI', code: 'BTC'}, 
+        { name: 'DAI', code: 'DAI'}, 
         { name: 'Solana', code: 'SOL'}, 
         { name: 'USDT', code: 'USDT'},
       ]
@@ -46,32 +46,33 @@ export default {
       let exchangeWinner = "";
 
       if (exchangeList) {
-      if( this.action == 'purchase') {
-        // totalAsk = Precio de compra final incluyendo las comisiones de transferencia y trade
-        // si quiero comprar tengo que buscar el menor precio
-        Object.keys(exchangeList).forEach((key) => {
-          if (exchangeList[key].totalAsk < minPriceInMarket && exchangeList[key].totalAsk != 0) {
-            minPriceInMarket = exchangeList[key].totalAsk;   
-            exchangeWinner = key;
-          }
-        });
-        this.fiatAmount = (minPriceInMarket * this.cryptoAmount).toFixed(2)
-      console.log(`Crypto de ${ exchangeWinner } al mejor precio: ${ minPriceInMarket }`);
-      }
+          
+        if( this.action == 'purchase') {
+          // totalAsk = Precio de compra final incluyendo las comisiones de transferencia y trade
+          // si quiero comprar tengo que buscar el menor precio
+          Object.keys(exchangeList).forEach((key) => {
+            if (exchangeList[key].totalAsk < minPriceInMarket && exchangeList[key].totalAsk != 0) {
+              minPriceInMarket = exchangeList[key].totalAsk;   
+              exchangeWinner = key;
+            }
+          });
+          this.fiatAmount = (minPriceInMarket * this.cryptoAmount).toFixed(2)
+        console.log(`Crypto de ${ exchangeWinner } al mejor precio: ${ minPriceInMarket }`);
+        }
 
-      // totalBid = Precio de venta final incluyendo las comisiones de transferencia y trade
-      if( this.action == 'sale') {
-        // si quiero vender tengo que buscar el mayor precio
-        Object.keys(exchangeList).forEach((key) => {
-          if (exchangeList[key].totalBid > maxPriceInMarket && exchangeList[key].totalBid != 0) {
-            maxPriceInMarket = exchangeList[key].totalBid;   
-            exchangeWinner = key;
-          }
-        });
-        this.fiatAmount = (maxPriceInMarket * this.cryptoAmount).toFixed(2)
-      console.log(`Crypto de ${ exchangeWinner } al mejor precio: ${ maxPriceInMarket }`);
-      }
-      this.exchangeWinnerPrice =  exchangeWinner.toUpperCase();
+        // totalBid = Precio de venta final incluyendo las comisiones de transferencia y trade
+        if( this.action == 'sale') {
+          // si quiero vender tengo que buscar el mayor precio
+          Object.keys(exchangeList).forEach((key) => {
+            if (exchangeList[key].totalBid > maxPriceInMarket && exchangeList[key].totalBid != 0) {
+              maxPriceInMarket = exchangeList[key].totalBid;   
+              exchangeWinner = key;
+            }
+          });
+          this.fiatAmount = (maxPriceInMarket * this.cryptoAmount).toFixed(2)
+          console.log(`Crypto de ${ exchangeWinner } al mejor precio: ${ maxPriceInMarket }`);
+        }
+        this.exchangeWinnerPrice =  exchangeWinner.toUpperCase();
       }
       return;
     },
